578b0882fd47f8fc7988bfb197259b5155fa78c8
jasminexie <ameliasgoldfish@gmail.com>
2019-05-10 11:44:32 +0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+
+const extensions = [
+  '.js', '.jsx', '.ts', '.tsx',
+];
+
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd',
+  },
+  plugins:
+   [
+     resolve({ extensions }),
+     babel({
+       extensions,
+       runtimeHelpers: true,
+       exclude: 'node_modules/**',
+       include: ['src/**/*'],
+     }),
+     commonjs({
+       include: 'node_modules/**',
+     }),
+     replace({
+       'process.env.NODE_ENV': '"production"',
+     }),
+     uglify(),
+   ],
+};
